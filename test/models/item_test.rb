3abf9a557eaec00d27fac43380f3e2401065b154
Luis Fernando Jimenez <lu.jim@protonmail.com>
2025-05-22 11:43:52 +0200

    feat(Item) - unit test

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = Item.new(
+      content: "Test Item",
+      user: users(:one)
+    )
+  end
+
+  test "should save item" do
+    assert @item.save, "Failed to save the item"
+  end
+
+  test "should not save item without user" do
+    @item.user = nil
+    assert_not @item.save, "Saved the item without a user"
+  end
 end
