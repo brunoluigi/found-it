d56f03002fd18503e6db613446bdaa261273d811
Luis Fernando Jimenez <lu.jim@protonmail.com>
2025-05-25 21:52:56 +0200

    feat(tests) - create system tests for models

---
+++ test/system/item_views_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ItemViewsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit item_views_url
+  #
+  #   assert_selector "h1", text: "ItemViews"
+  # end
+end
